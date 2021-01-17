5591187f683aee5c04b044ff1b789bfb5953d678
Daniel12346 <danezoki2@gmail.com>
2021-01-18 00:12:24 +0100

    ispis infiks izraza sa zagradama

--- zad10.c
+++ zad10.c
@@ -3,12 +3,16 @@
 #include<string.h>
 #define ERR -1
 #define MAX 100
+#define NOT_SET -2
+
+//brojevi mogu biti višeznamenkasti pa ih se u zapisu u datoteci treba odvajati razmakom
+//npr. 3+445 =>   3 445 +
 
 typedef struct _stablo_cvor StabloCvor;
 typedef StabloCvor *Stablo;
 struct _stablo_cvor
 {
-    char* el;
+    char el[MAX];
     Stablo L;
     Stablo D;
 };
@@ -24,6 +28,33 @@ struct _cvor
 
 
 
+int NemaPodstabla(Stablo s){
+    if (s == NULL) return ERR;
+    return s->L == NULL && s->D == NULL;
+}
+
+int IspisInorder(Stablo s){
+    if (s == NULL) return 0;
+    if (s->L == NULL || NemaPodstabla(s->L)){ 
+       IspisInorder(s->L);
+    }else{
+    	printf("(");
+    	IspisInorder(s->L);
+    	printf(")");
+    }
+
+    printf("%s", s->el);
+
+    if (s->D == NULL || NemaPodstabla(s->D)){ 
+       IspisInorder(s->D);
+    }else{
+    	printf("(");
+    	IspisInorder(s->D);
+    	printf(")");
+    }
+}
+
+
 Pozicija StvoriPoziciju(Stablo s)
 {
     Pozicija p = malloc(sizeof(Cvor));
@@ -31,6 +62,7 @@ Pozicija StvoriPoziciju(Stablo s)
         return NULL;
     p->next = NULL;
     p->stablo = s;
+    return p;
 }
 
 int Push(Pozicija stog, Stablo s){
@@ -41,17 +73,22 @@ int Push(Pozicija stog, Stablo s){
     stog->next = p;
 }
 
+
 Stablo Pop(Pozicija stog){
+    Stablo s = NULL;
     Pozicija tmp = NULL;
     if (stog == NULL) return NULL;
     tmp = stog->next;
     stog->next = tmp->next;
+    s = tmp->stablo;
+   // IspisInorder(s);
     free(tmp);
+    return s; 
 }
 
 
 
-Pozicija StvoriStablo(char* el)
+Stablo StvoriStablo(char* el)
 {
     Stablo s = malloc(sizeof(StabloCvor));
     if (s == NULL)
@@ -59,18 +96,89 @@ Pozicija StvoriStablo(char* el)
     s->L = NULL;
     s->D = NULL;
     strcpy(s->el, el);
+    return s;
 }
+
 
 int main()
 {
     FILE *fp = fopen("postfiks.txt", "r");
-    Pozicija stog = NULL;
-    Stablo stablo = NULL;
-    char buff[100];
-    int count = 0, fsize = 0;
+    Stablo stablo = NULL, s1 = NULL, s2 = NULL, tmp_stablo = NULL;
+    Pozicija stog = StvoriPoziciju(tmp_stablo);
+    char buff[MAX], str[MAX], tmp_str[2];
+    int count = 0, fsize = 0, tmp = NOT_SET;
     fseek(fp, 0, SEEK_END);
     fsize = ftell(fp);
     rewind(fp);
     fread(buff, 1, fsize, fp);
-    while(){}
+    //TODO: ovo u novu funkciju
+    for (int i = 0; i < fsize; i++){
+        switch(buff[i]){
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		    //sastavljanje broja od vise znakova: sa svakim novim znakom koji predstavlja znamenku, broj koji se sastavlja se množi sa 10, a najnovija znamenka (novi znak) ide na zadnje mjesto
+		    if (tmp == NOT_SET){
+		      tmp = buff[i] - '0';
+		    }else{
+		      tmp = 10 * tmp + buff[i] - '0';	
+		    }
+		    break;
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+
+	
+			//ako naiđemo na prazninu, prestaje sastavljanje broja i taj postaje element stabla koje ide na stog
+	                if (tmp != NOT_SET){
+		            //stvaranje stringa od int elementa
+			    sprintf(str, "%d", tmp);
+			    tmp_stablo = StvoriStablo(str);
+			    if (tmp_stablo == NULL){
+				return ERR;	
+			    }
+			    Push(stog, tmp_stablo);
+			};
+			tmp = NOT_SET;
+
+
+
+			//sastavljanje stringa od operanda i znaka za kraj stringa
+			//jer cvor stabla sadrzi char*, a ne sami char
+			tmp_str[0] = buff[i];
+			tmp_str[1] = '\0';
+			s2 = Pop(stog);
+			s1 = Pop(stog);
+			if (s1 == NULL || s2 == NULL){
+			    printf("Greska u datoteci\n");
+			    return ERR;
+			};
+			tmp_stablo = StvoriStablo(tmp_str);
+			if (tmp_stablo == NULL) return ERR;
+			tmp_stablo->L = s1;
+			tmp_stablo->D = s2;
+			Push(stog, tmp_stablo);
+		default:  
+	                if (tmp != NOT_SET){
+			    sprintf(str, "%d", tmp);
+			    tmp_stablo = StvoriStablo(str);
+			    if (tmp_stablo == NULL){
+				return ERR;	
+			    }
+			    Push(stog, tmp_stablo);
+			};
+			tmp = NOT_SET;
+			break;
+	}	
+    }
+    stablo = Pop(stog);
+    IspisInorder(stablo);
 }
