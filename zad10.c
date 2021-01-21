7d13615751f74b06e954211ca5243d6824baa62f
Daniel12346 <danezoki2@gmail.com>
2021-01-21 13:21:41 +0100

    popravljen ispis u datoteku

--- zad10.c
+++ zad10.c
@@ -28,76 +28,13 @@ struct _cvor
 
 
 
-int NemaPodstabla(Stablo s){
-    if (s == NULL) return ERR;
-    return s->L == NULL && s->D == NULL;
-}
+int NemaPodstabla(Stablo);
+int IspisInorderUString(Stablo, char*);
+Pozicija StvoriPoziciju(Stablo);
+Stablo Pop(Pozicija);
+int Push(Pozicija, Stablo);
+Stablo StvoriStablo(char*);
 
-int IspisInorderUString(Stablo s, char* str){
-    if (s == NULL) return 0;
-    if (s->L == NULL || NemaPodstabla(s->L)){ 
-       IspisInorderUString(s->L, str);
-    }else{
-    	sprintf(str + strlen(str), "(");
-    	IspisInorderUString(s->L, str + strlen(str));
-    	sprintf(str + strlen(str), ")");
-    }
-
-    sprintf(str + strlen(str), "%s", s->el);
-
-    if (s->D == NULL || NemaPodstabla(s->D)){  
-    	IspisInorderUString(s->D, str);
-    }else{
-    	sprintf(str + strlen(str), "(");
-    	IspisInorderUString(s->D, str + strlen(str));
-    	sprintf(str + strlen(str), ")");
-    }
-}
-
-
-Pozicija StvoriPoziciju(Stablo s)
-{
-    Pozicija p = malloc(sizeof(Cvor));
-    if (p == NULL)
-        return NULL;
-    p->next = NULL;
-    p->stablo = s;
-    return p;
-}
-
-int Push(Pozicija stog, Stablo s){
-    if (stog == NULL) return ERR;
-    Pozicija p = StvoriPoziciju(s);
-    if (p == NULL) return ERR;
-    p->next = stog->next;
-    stog->next = p;
-}
-
-
-Stablo Pop(Pozicija stog){
-    Stablo s = NULL;
-    Pozicija tmp = NULL;
-    if (stog == NULL) return NULL;
-    tmp = stog->next;
-    stog->next = tmp->next;
-    s = tmp->stablo;
-   // IspisInorder(s);
-    free(tmp);
-    return s; 
-}
-
-
-
-Stablo StvoriStablo(char* el)
-{
-    Stablo s = malloc(sizeof(StabloCvor));
-    if (s == NULL)
-        return NULL;
-    s->L = NULL;
-    s->D = NULL;
-    strcpy(s->el, el);
-    return s;
-}
 
 
 int main()
@@ -184,8 +121,82 @@ int main()
 	}	
     }
     stablo = Pop(stog);
-    /* str_dat - 1 jer printf u rekurzivnoj funkciji preskace određeni broj mjesta u stringu pri svakom pozivu. Bez tog preskakanja bi uvijek upisivao u str[0] (na početak stringa) i funkcija ne bi radila. -1 ovdje kompenzira preskok u prvom pozivu printf u toj funkciji*/
-    IspisInorderUString(stablo, str_dat - 1);
-    fputs(str_dat, fp2);
+    if (stablo == NULL) return ERR;
+    IspisInorderUString(stablo, str_dat);
+    /* str_dat + 6 jer printf u rekurzivnoj funkciji preskace određeni broj mjesta u stringu pri svakom pozivu. Bez tog preskakanja bi uvijek upisivao u str[0] (na početak stringa) i funkcija ne bi radila. +6 ovdje kompenzira preskok u prvom pozivu printf u toj funkciji*/
+    fputs(str_dat + 6, fp2);
     fclose(fp2);
+}
+
+
+int NemaPodstabla(Stablo s){
+    if (s == NULL) return ERR;
+    return s->L == NULL && s->D == NULL;
+}
+
+int IspisInorderUString(Stablo s, char* str){
+    if (s == NULL) return 0;
+    if (s->L == NULL || NemaPodstabla(s->L)){ 
+       IspisInorderUString(s->L, str);
+    }else{
+    	sprintf(str + strlen(str), "(");
+    	IspisInorderUString(s->L, str + strlen(str));
+    	sprintf(str + strlen(str), ")");
+    }
+
+    sprintf(str + strlen(str), "%s", s->el);
+
+    if (s->D == NULL || NemaPodstabla(s->D)){  
+    	IspisInorderUString(s->D, str);
+    }else{
+    	sprintf(str + strlen(str), "(");
+    	IspisInorderUString(s->D, str + strlen(str));
+    	sprintf(str + strlen(str), ")");
+    }
+}
+
+
+Pozicija StvoriPoziciju(Stablo s)
+{
+    Pozicija p = malloc(sizeof(Cvor));
+    if (p == NULL)
+        return NULL;
+    p->next = NULL;
+    p->stablo = s;
+    return p;
+}
+
+int Push(Pozicija stog, Stablo s){
+    if (stog == NULL) return ERR;
+    Pozicija p = StvoriPoziciju(s);
+    if (p == NULL) return ERR;
+    p->next = stog->next;
+    stog->next = p;
+}
+
+
+Stablo Pop(Pozicija stog){
+    Stablo s = NULL;
+    Pozicija tmp = NULL;
+    if (stog == NULL) return NULL;
+    tmp = stog->next;
+    if (tmp == NULL) return NULL;
+    stog->next = tmp->next;
+    s = tmp->stablo;
+   // IspisInorder(s);
+    free(tmp);
+    return s; 
+}
+
+
+
+Stablo StvoriStablo(char* el)
+{
+    Stablo s = malloc(sizeof(StabloCvor));
+    if (s == NULL)
+        return NULL;
+    s->L = NULL;
+    s->D = NULL;
+    strcpy(s->el, el);
+    return s;
 }
